f526b53154b75ee6236335c5d3bae733d2f5cd1f
Sannie Wan <sanniewan@gmail.com>
2023-03-23 22:04:50 -0700

    add deploy and Robot, add README

---
+++ MegadonLo2023/nav/deploy.py
@@ -0,0 +1,11 @@
+from MegadonLo2023.nav.Robot.Robot import Robot
+from MegadonLo2023.nav.Teleop.Teleop import Teleop
+
+
+'''
+ initializes and creates Robot, Teleop object, begins teleop loop
+'''
+
+
+def run(queue_in, queue_out):
+    pass
---
+++ README.md
@@ -0,0 +1,13 @@
+Welcome to the repository for the Megalodon MATE ROV robotics team of 2023.
+
+Team members:
+
+Alex L.
+Hannah H.
+Jiaqi J.
+Martin Z.
+Sarah Y.
+Vishal D.
+
+Valerie F.
+Sannie W.
